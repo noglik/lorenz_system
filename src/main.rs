3524ccf46a7c31907f11cd222f854f76406629d3
noglik <yaroslaubuk@gmail.com>
2021-01-19 23:48:49 +0000

    Use 4 chanels rgba type
    It will allow to play with transparency.
    Little bit of tidying up(no more warnings 😉)

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,51 @@
 extern crate image;
 
-const sigma: f64 = 10.0;
-const rho: f64 = 28.0;
-const beta: f64 = 8.0 / 3.0;
+use image::{ImageBuffer, Rgba};
 
-const dt: f64 = 0.0001;
+const SIGMA: f64 = 10.0;
+const RHO: f64 = 28.0;
+const BETA: f64 = 8.0 / 3.0;
 
-const background: u8 = 30;
+const WIDTH: u32 = 1920;
+const HEIGHT: u32 = 1080;
+
+const BACKGROUND: [u8; 4] = [30, 30, 30, 255];
+
+const SCALE: f64 = 20.0;
 
 fn main() {
     // use some random data
     let mut x: f64 = 1.0;
     let mut y: f64 = 0.0;
     let mut z: f64 = 0.0;
+    let dt = 0.0001;
 
-    let imgx = 1920;
-    let imgy = 1080;
+    let r: u8 = 100;
+    let g: u8 = 100;
+    let b: u8 = 100;
 
-    // Create a new ImgBuf with width: imgx and height: imgy
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+    // Create a new ImgBuf
+    let mut imgbuf: ImageBuffer<Rgba<u8>, Vec<u8>> = ImageBuffer::new(WIDTH, HEIGHT);
 
-    // Iterate over the coordinates and pixels of the image
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        *pixel = image::Rgb([background, background, background]);
+    // Fill with background
+    for (_x, _y, pixel) in imgbuf.enumerate_pixels_mut() {
+        *pixel = Rgba(BACKGROUND);
     }
 
-    let mut r: u8 = 100;
-    let mut g: u8 = 100;
-    let mut b: u8 = 100;
+    for _n in 1..600000 {
+        // Lorenz system
+        x = (SIGMA * (y - x)) * dt + x;
+        y = (x * (RHO - z) - y) * dt + y;
+        z = (x * y - BETA * z) * dt + z;
 
-    for _n in 1..900000 {
-        let dx = (sigma * (y - x)) * dt;
-        let dy = (x * (rho - z) - y) * dt;
-        let dz = (x * y - beta * z) * dt;
-        x = dx + x;
-        y = dy + y;
-        z = dz + z;
+        let coordinate_x = (x * SCALE + WIDTH as f64 / 2.0) as u32;
+        let coordinate_y = (y * SCALE + HEIGHT as f64 / 2.0) as u32;
 
-        let pixel = imgbuf.get_pixel_mut((x * 19.0 + 960.0) as u32, (y * 19.0 + 540.0) as u32);
-        *pixel = image::Rgb([r, g, b]);
-
-        // if r > 0 && b == 55 {
-        //     r = r - 1;
-        //     g = g + 1;
-        // }
-
-        // if g > 0 && r == 55 {
-        //     g = g - 1;
-        //     b = b + 1;
-        // }
-
-        // if b > 0 && g == 55 {
-        //     r = r + 1;
-        //     b = b - 1;
-        // }
+        // skip out of bounds coordinates
+        if coordinate_x < WIDTH && coordinate_y < HEIGHT {
+            let pixel = imgbuf.get_pixel_mut(coordinate_x, HEIGHT - coordinate_y);
+            *pixel = Rgba([r, g, b, 255]);
+        }
     }
 
     imgbuf.save("lorenz_system.png").unwrap()
